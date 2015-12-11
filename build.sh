05a9f1122a441a3a39e5004ec459cf64832a768f
David Andrews <dandrews@edgecast.com>
2015-12-10 17:45:25 -0800

    Rework cmake files to make the package dependency a little better and link as much statically during build-time as possible.
    Squashed commit of the following:
    
    commit c12741dd6d14f3d79f28fde79921417ac765be76
    Merge: 4a5f337 f2b7799
    Author: David Andrews <dandrews@edgecast.com>
    Date:   Thu Dec 10 17:44:55 2015 -0800
    
        Merge master in
    
    commit 4a5f337076424ddc916a89f51c75f938b2a98528
    Author: David Andrews <dandrews@edgecast.com>
    Date:   Thu Dec 10 17:42:21 2015 -0800
    
        This works, and has very few runtime dependencies
    
    commit e7f5564d0f43130d76eceefc295c1d2f03b48830
    Author: David Andrews <dandrews@edgecast.com>
    Date:   Wed Dec 9 18:03:40 2015 -0800
    
        Initial work reorganizing cmake to try to:
        1. Deprecate the hackish build.sh script package dependency.
        2. Link against as much as possible statically.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 # All Rights Reserved
 #
 #   Author: Reed P Morrison
-#   Date:   02/07/2014  
+#   Date:   02/07/2014
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -19,34 +19,6 @@
 #   limitations under the License.
 #
 # ------------------------------------------------------------------------------
-
-# ------------------------------------------------------------------------------
-# Requirements:
-#   libssl-dev
-#   libgoogle-perftools-dev
-# ------------------------------------------------------------------------------
-BUILD_DEPENDS="$(grep BUILDS_DEPENDS CMakeLists.txt | sed -e 's/.*"\(.*\)".*/\1/')"
-echo "Checking build dependencies (manually):"
-
-while read pkg; do
-    echo -n "  $pkg..."
-
-    pkg_name="${pkg%% *}"
-    pkg_ver="${pkg##* }"
-    pkg_ver="$(echo "${pkg_ver}" | sed -e 's/.*([<>=]*\([0-9].*\))/\1/')"
-    pkg_op="$(echo "${pkg_ver}" | sed -e 's/^\([^0-9]*\).*/\1/')"
-
-    if [[ "${pkg_ver}" == "${pkg_name}" ]]; then pkg_ver="0.0.0"; fi
-
-    installed_ver="$(dpkg-query --show --showformat '${Version}' $pkg_name)"
-    if [[ $? == 0 ]] &&
-        dpkg --compare-versions "${pkg_ver}" le "${installed_ver}"; then
-        echo "  pass"
-    else
-        echo "  fail.  Please install this package: ${pkg_name} with version ${pkg_ver}"
-        exit 1
-    fi
-done <<<"$(echo "${BUILD_DEPENDS}" | tr ',' '\n')"
 
 # ------------------------------------------------------------------------------
 # To build...
