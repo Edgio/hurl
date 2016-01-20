a6feaf77a9349776bfdc37c37c97e5960566cfb3
Reed Morrison <rmorrison@edgecast.com>
2016-01-19 17:06:24 -0800

    Adding response code.

--- src/core/hlx/api_resp.cc
+++ src/core/hlx/api_resp.cc
@@ -60,6 +60,9 @@ int32_t nbq_write_status(nbq &ao_q, http_status_t a_status)
         if(i_r != http_resp_strs::S_CODE_RESP_MAP.end())
         {
                 ao_q.write("HTTP/1.1 ", strlen("HTTP/1.1 "));
+                char l_status_code_str[10];
+                sprintf(l_status_code_str, "%u ", a_status);
+                ao_q.write(l_status_code_str, strnlen(l_status_code_str, 10));
                 ao_q.write(i_r->second.c_str(), i_r->second.length());
                 ao_q.write("\r\n", strlen("\r\n"));
         }
