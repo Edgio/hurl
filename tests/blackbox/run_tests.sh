752ffd3fd72eff9ecab8916ce3dbcd74717986f9
David Andrews <dandrews@edgecast.com>
2016-01-19 16:13:16 -0800

    Link blackbox tests into the build system

--- tests/blackbox/run_tests.sh
+++ tests/blackbox/run_tests.sh
@@ -3,4 +3,5 @@
 # Run tests with nose
 # ==============================================================================
 #nosetests -w ./tests/blackbox --with-progressive --nocapture --processes=1 --process-timeout=600
-nosetests -w ./tests/blackbox --with-progressive --processes=1 --process-timeout=600
+TESTS_LOCATION=$(dirname $0)
+nosetests -w ${TESTS_LOCATION} --with-progressive --processes=1 --process-timeout=600
