29496b4c66dc852dc68cc2c9aad5ae4757246cba
Reed Morrison <rmorrison@edgecast.com>
2016-02-16 18:42:43 -0800

    Adding placeholder for nconn_pool test.

---
+++ tests/whitebox/nconn/wb_nconn_pool.cc
@@ -0,0 +1,40 @@
+//: ----------------------------------------------------------------------------
+//: Copyright (C) 2015 Verizon.  All Rights Reserved.
+//: All Rights Reserved
+//:
+//: \file:    wb_nconn_pool.cc
+//: \details: TODO
+//: \author:  Reed P. Morrison
+//: \date:    02/16/2016
+//:
+//:   Licensed under the Apache License, Version 2.0 (the "License");
+//:   you may not use this file except in compliance with the License.
+//:   You may obtain a copy of the License at
+//:
+//:       http://www.apache.org/licenses/LICENSE-2.0
+//:
+//:   Unless required by applicable law or agreed to in writing, software
+//:   distributed under the License is distributed on an "AS IS" BASIS,
+//:   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//:   See the License for the specific language governing permissions and
+//:   limitations under the License.
+//:
+//: ----------------------------------------------------------------------------
+
+//: ----------------------------------------------------------------------------
+//: Includes
+//: ----------------------------------------------------------------------------
+#include "catch/catch.hpp"
+#include "nconn_pool.h"
+
+//: ----------------------------------------------------------------------------
+//: Tests
+//: ----------------------------------------------------------------------------
+TEST_CASE( "nconn pool test", "[nconn_pool]" )
+{
+        SECTION("Basic Connection Pool Test")
+        {
+                ns_hlx::nconn_pool l_p(16);
+                REQUIRE(l_p.num_free() == 16);
+        }
+}
