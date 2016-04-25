521d0ff6ff79fc719bd9e8201d34952352cb3f22
Reed Morrison <rmorrison@edgecast.com>
2016-04-24 21:50:45 -0700

    Adding keep-alives to python test server.

--- tests/blackbox/handlers/slow_http_server.py
+++ tests/blackbox/handlers/slow_http_server.py
@@ -12,7 +12,7 @@ G_SLOW_HTTP_SERVER_PORT = 12346
 class http_handler(BaseHTTPRequestHandler):
     #Handler for the GET requests
     def do_GET(self):
-        time.sleep(2)
+        time.sleep(0.5)
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.send_header('Content-length',len('monkeys are crazy!'))
@@ -21,6 +21,7 @@ class http_handler(BaseHTTPRequestHandler):
         return
 try:
     l_server = HTTPServer(('', G_SLOW_HTTP_SERVER_PORT), http_handler)
+    l_server.protocol_version = 'HTTP/1.1'
     l_server.serve_forever()
 except KeyboardInterrupt:
     print '^C received, shutting down the web server'
