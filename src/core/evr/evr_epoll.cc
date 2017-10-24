6b7e17b3fa726f31cd8e417f01429d7ffce94e1a
tinselcity <reed.morrison@gmail.com>
2017-10-24 10:19:52 -0700

    Trying EPOLLEXCLUSIVE if available.

--- src/core/evr/evr_epoll.cc
+++ src/core/evr/evr_epoll.cc
@@ -50,9 +50,12 @@ evr_epoll::evr_epoll(void):
                 printf("Error: epoll_create() failed: %s\n", strerror(errno));
                 exit(-1);
         }
-
+        int l_flags = EFD_NONBLOCK;
+#ifdef EPOLLEXCLUSIVE
+        l_flags |= EPOLLEXCLUSIVE;
+#endif
         // Create ctrl fd
-        m_ctrl_fd = eventfd(0, EFD_NONBLOCK);
+        m_ctrl_fd = eventfd(0, l_flags);
         if (m_ctrl_fd == -1)
         {
                 printf("Error: eventfd() failed: %s\n", strerror(errno));
