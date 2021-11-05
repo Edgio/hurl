9e36bb30321e35dde9e9a1f3f1f25c437adf62ff
tinselcity <reed.morrison@gmail.com>
2021-11-04 19:48:56 -0700

    org update.

--- src/core/status.h
+++ src/core/status.h
@@ -1,5 +1,5 @@
 //! ----------------------------------------------------------------------------
-//! Copyright Verizon.
+//! Copyright Edgecast Inc.
 //!
 //! \file:    status.h
 //! \details: TODO
