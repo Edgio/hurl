02cb5bb1c99f2b261f031ce93b7e65392f166801
Reed Morrison <reed.morrison@gmail.com>
2022-08-16 15:27:34 -0700

    Fixes for 22 04 (#49)
    * 22.04 work...
    
    * 22.04 work...
    
    * 22.04 work...

--- ext/udns-0.4/udns_resolver.c
+++ ext/udns-0.4/udns_resolver.c
@@ -1244,7 +1244,6 @@ static void dns_resolve_cb(struct dns_ctx *ctx, void *result, void *data) {
   struct dns_resolve_data *d = data;
   d->dnsrd_result = result;
   d->dnsrd_done = 1;
-  ctx = ctx;
 }
 
 void *dns_resolve(struct dns_ctx *ctx, struct dns_query *q) {
